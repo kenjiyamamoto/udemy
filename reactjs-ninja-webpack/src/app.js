cd6b3865f74ea8de966b9bb25fdc6f38c0dee220
Kenji Yamamoto <kenji@corp.globo.com>
2018-06-01 17:36:04 -0300

    treinamentos Udemy - First Commit

---
+++ reactjs-ninja-webpack/src/app.js
@@ -0,0 +1,12 @@
+'use strict'
+
+var React = require('react')
+var createReactClass = require('create-react-class')
+
+var Title = createReactClass({
+	render: function () {
+		return React.createElement('h1', null, 'Titulo')
+	}
+})
+
+module.exports = Title
